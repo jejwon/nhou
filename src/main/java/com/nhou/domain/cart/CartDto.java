45ae4200cec152f41730e97d28a75614a0b33665
jejwon <110441664+jejwon@users.noreply.github.com>
2023-01-16 20:58:55 +0900

    cart 만드는 중 222 (#50)

--- src/main/java/com/nhou/domain/cart/CartDto.java
+++ src/main/java/com/nhou/domain/cart/CartDto.java
@@ -1,10 +1,23 @@
 package com.nhou.domain.cart;
 
+import lombok.Data;
+
+@Data
 public class CartDto {
 
+	//cart
 	private int cartId;
 	private int product_productId;
 	private String member_userId;
 	private int count;
 
+	//product
+	private String productName;
+	private String color;
+	private String size;
+	private int price;
+	private int stock;
+	
+	//추가
+	private int totalPrice;
 }
--- src/main/java/com/nhou/service/cart/CartService.java
+++ src/main/java/com/nhou/service/cart/CartService.java
@@ -1,11 +1,21 @@
 package com.nhou.service.cart;
 
 
+import java.util.List;
+
 import com.nhou.domain.cart.CartDto;
 
 public interface CartService {
 	
 	int insert(CartDto cart);
+
+	int delete(int cartId);
+
+	int update(CartDto cart);
+
+	CartDto getByCartId(int cartId);
+
+	List<CartDto> list();
 
 
 }
