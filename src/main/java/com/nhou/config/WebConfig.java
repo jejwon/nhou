e13c6783495f078f8c1a909d74361321a5cc1ccd
suj00033 <suj00033@naver.com>
2023-01-31 09:39:57 +0900

    Revert "변경"
    This reverts commit 6f4a7902514568d711adc5e15b6d1ec7d2f27644.

--- src/main/java/com/nhou/config/WebConfig.java
+++ src/main/java/com/nhou/config/WebConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
-	/*private String connectPath = "/image/**";
+	private String connectPath = "/image/**";
 	private String resourcePath = "file:///C:\\Users\\fgsfs\\Desktop\\study\\upload\\nhou\\store\\/";
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler(connectPath)
-				.addResourceLocations(resourcePath);*/
-	// }
+				.addResourceLocations(resourcePath);
+	}
 }
